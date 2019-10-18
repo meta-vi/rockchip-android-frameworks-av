ddeb147e70eaa76271590ea01ef3f337e75bdf77
Zhou Jing <tracy.zhou@rock-chips.com>
2019-10-18 14:54:46 +0800

    MediaPlayerFactory: choose nuplayer to play .apk by default
    Change-Id: Idfb7b9efdf64c1b29aa1c7498f0bc13094aeda98
    Signed-off-by: Zhou Jing <tracy.zhou@rock-chips.com>

--- media/libmediaplayerservice/MediaPlayerFactory.cpp
+++ media/libmediaplayerservice/MediaPlayerFactory.cpp
@@ -39,6 +39,25 @@ Mutex MediaPlayerFactory::sLock;
 MediaPlayerFactory::tFactoryMap MediaPlayerFactory::sFactoryMap;
 bool MediaPlayerFactory::sInitComplete = false;
 
+static status_t getFileName(int fd,String8 *FilePath) {
+    static ssize_t link_dest_size;
+    static char link_dest[PATH_MAX];
+    const char *ptr = NULL;
+    String8 path;
+    path.appendFormat("/proc/%d/fd/%d", getpid(), fd);
+    if ((link_dest_size = readlink(path.string(), link_dest, sizeof(link_dest)-1)) < 0) {
+        return errno;
+    } else {
+        link_dest[link_dest_size] = '\0';
+    }
+
+    path = link_dest;
+    ptr = path.string();
+    *FilePath = String8(ptr);
+
+    return OK;
+}
+
 status_t MediaPlayerFactory::registerFactory_l(IFactory* factory,
                                                player_type type) {
     if (NULL == factory) {
@@ -111,6 +130,11 @@ player_type MediaPlayerFactory::getPlayerType(const sp<IMediaPlayer>& client,
                                               int fd,
                                               int64_t offset,
                                               int64_t length) {
+    String8 filePath;
+    getFileName(fd,&filePath);
+    if (strstr(filePath.string(),".apk")) {
+        return NU_PLAYER;
+    }
     GET_PLAYER_TYPE_IMPL(client, fd, offset, length);
 }
 
