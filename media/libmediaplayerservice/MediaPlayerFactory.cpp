57ab690b7ed5bf0cd238d90f3f28529395d71343
Zhou Jing <tracy.zhou@rock-chips.com>
2019-11-22 17:20:07 +0800

    [MediaPlayerFactory]: fix handwrite error
    Change-Id: Ic5ddb6f9cee9d9a8ac1c8db497c78e4ddfc56129
    Signed-off-by: Zhou Jing <tracy.zhou@rock-chips.com>

--- media/libmediaplayerservice/MediaPlayerFactory.cpp
+++ media/libmediaplayerservice/MediaPlayerFactory.cpp
@@ -132,8 +132,6 @@ void MediaPlayerFactory::unregisterFactory(player_type type) {
 
 player_type MediaPlayerFactory::getPlayerType(const sp<IMediaPlayer>& client,
                                               const char* url) {
-    String8 filePath;
-    getFileName(fd,&filePath);
     if (strstr(url,".apk") || strstr(url,".ogg")) {
         return NU_PLAYER;
     }
