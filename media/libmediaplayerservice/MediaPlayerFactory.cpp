b87bdef99ad9e12a6645866279332e7e694071ed
He Hua <hh@rock-chips.com>
2020-01-09 17:28:53 +0800

    [mediaserver]: use rockit to play xx.apk default
    Change-Id: Ibc8e8fccef2453279143cf1dd34518440959094d
    Signed-off-by: He Hua <hh@rock-chips.com>

--- media/libmediaplayerservice/MediaPlayerFactory.cpp
+++ media/libmediaplayerservice/MediaPlayerFactory.cpp
@@ -132,7 +132,7 @@ void MediaPlayerFactory::unregisterFactory(player_type type) {
 
 player_type MediaPlayerFactory::getPlayerType(const sp<IMediaPlayer>& client,
                                               const char* url) {
-    if (strstr(url,".apk") || strstr(url,".ogg")) {
+    if (strstr(url,".ogg")) {
         return NU_PLAYER;
     }
     GET_PLAYER_TYPE_IMPL(client, url);
@@ -144,8 +144,7 @@ player_type MediaPlayerFactory::getPlayerType(const sp<IMediaPlayer>& client,
                                               int64_t length) {
     String8 filePath;
     getFileName(fd,&filePath);
-    if (strstr(filePath.string(),".apk")
-        || strstr(filePath.string(), ".ogg")
+    if (strstr(filePath.string(), ".ogg")
         || strstr(filePath.string(), "notification_sound_cache")
         || strstr(filePath.string(), "ringtone_cache")
         || strstr(filePath.string(), "alarm_alert_cache")) {
